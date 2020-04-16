1275718279de3e6c2c4efdaa792e36693fec320c
evandro <maddesevandro@gmail.com>
2020-04-16 19:20:15 +0200

    Added color to member for computation reason

--- main.c
+++ main.c
@@ -8,27 +8,35 @@ typedef enum {
     false, true
 } bool;
 
+typedef enum {
+    red, blue, white
+} type;
+
 //name of each member
 typedef char name[40];
 
 // puntatore  a nome di un membro
-typedef name *splitFrom; 
+typedef name *splitFrom;
 
 //each member has a name, a list of "enemy" members and the size of this last list 
 typedef struct member {
     name memberID;
     splitFrom *split;//array di puntatore a nome di membri
     int splitSize;
+    type color;
 };
 
-char addMember(char fromUsr) {
-
-
+//Following is a simple algorithm to find out whether a given graph is Birpartite or not using Breadth First Search (BFS).
+//1. Assign RED color to the source vertex (putting into set U).
+//2. Color all the neighbors with BLUE color (putting into set V).
+//3. Color all neighbor’s neighbor with RED color (putting into set U).
+//4. This way, assign color to all vertices such that it satisfies all the constraints of m way coloring problem where m = 2.
+//5. While assigning colors, if we find a neighbor which is colored with same color as current vertex, then the graph cannot be colored with 2 vertices (or graph is not Bipartite)
+bool isBipartite() {
+    int c = 0;
+    return true;
 }
 
-void addsplit(struct member currentMember) {
-
-}
 //for each test this function adds the members and thier relations to the data structure
 struct member *addPairs(int numberPairs) {
     char line[100];
@@ -38,7 +46,7 @@ struct member *addPairs(int numberPairs) {
     struct member *testMembers = malloc(numberPairs * 2 *
                                         sizeof(struct member));//al messimo ho il numero di membri pari al doppio del numeor di coppie
 
-    for (int i = 0; i < numberPairs ; ++i) {
+    for (int i = 0; i < numberPairs; ++i) {
         insertOne = true;
         insertTwo = true;
         int posOne = 0, posTwo = posOne + 1;
@@ -64,6 +72,7 @@ struct member *addPairs(int numberPairs) {
             testMembers[currentPos].split = malloc(dim * sizeof(splitFrom));
             testMembers[currentPos].split[0] = &testMembers[currentPos + 1].memberID;
             testMembers[currentPos].splitSize = 1;
+            testMembers[currentPos].color = white;
             currentPos++;
 
             //member two
@@ -71,6 +80,7 @@ struct member *addPairs(int numberPairs) {
             testMembers[currentPos].split = malloc(dim * sizeof(splitFrom));
             testMembers[currentPos].split[0] = &testMembers[currentPos - 1].memberID;
             testMembers[currentPos].splitSize = 1;
+            testMembers[currentPos].color = white;
             currentPos++;
 
         }
@@ -81,6 +91,7 @@ struct member *addPairs(int numberPairs) {
             testMembers[currentPos].split = malloc(dim * sizeof(splitFrom));
             testMembers[currentPos].split[0] = &testMembers[posTwo].memberID;
             testMembers[currentPos].splitSize = 1;
+            testMembers[currentPos].color = white;
             currentPos++;
             //member two
             testMembers[posTwo].split[testMembers[posTwo].splitSize] = &testMembers[currentPos - 1].memberID;
@@ -96,6 +107,7 @@ struct member *addPairs(int numberPairs) {
             testMembers[currentPos].split = malloc(dim * sizeof(splitFrom));
             testMembers[currentPos].split[0] = &testMembers[posOne].memberID;
             testMembers[currentPos].splitSize = 1;
+            testMembers[currentPos].color = white;
             currentPos++;
         }
         //aggiungere solo relazione
@@ -120,13 +132,14 @@ int main() {
         fgets(line, 90, stdin);
         sscanf(line, "%d", &numberTest);
         currentCaseMembers = addPairs(numberTest);
-        printf("primo membro: %s split from: ", &currentCaseMembers[0].memberID);
+        printf("primo membro: %s; color: %d split from: ", &currentCaseMembers[0].memberID,
+               currentCaseMembers[0].color);
         for (int i = 0; i < currentCaseMembers[0].splitSize; ++i) {
             printf("%s\t", currentCaseMembers[0].split[i]);
         }
         printf("\n secondo membro: %s split from ", &currentCaseMembers[1].memberID);
         for (int i = 0; i < currentCaseMembers[1].splitSize; ++i) {
-            printf("%s\t", currentCaseMembers[1].split[i]);
+            printf("%s %d\t", currentCaseMembers[1].split[i], currentCaseMembers[1].split[i]);
         }
     }
     return 0;
